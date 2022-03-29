84d9a59bd47a2370b79df0c1bede1f93f84f8dc4
e-arlo-be <arloalb@bu.edu>
2022-03-29 13:24:06 -0400

    working config for VM

--- build.sh
+++
@@ -1,7 +0,0 @@
-export INSTALL_PATH=/home/boobee/myinstall
-export INSTALL_DTBS_PATH=/home/boobee/myinstall
-rm -rf /home/boobee/myinstall
-mkdir -p /home/boobee/myinstall
-CC=aarch64-linux-gnu-gcc make -j25 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
-CC=aarch64-linux-gnu-gcc make ARCH=arm64 INSTALL_MOD_PATH=/home/boobee/myinstall modules_install
-#CC=aarch64-linux-gnu-gcc make ARCH=arm64 install

c22ff85b38db12be6d51f7b18d448abaaf0e6acb
e-arlo-be <arloalb@bu.edu>
2022-04-25 16:06:14 -0400

    VM scripts

--- VMbuild.sh
+++ VMbuild.sh
@@ -3,10 +3,10 @@ export INSTALL_DTBS_PATH=/home/boobee/VMmyinstall
 rm -rf /home/boobee/VMmyinstall
 mkdir -p /home/boobee/VMmyinstall
 #CC=aarch64-linux-gnu-gcc ARCH=arm64 make defconfig
-CC=aarch64-linux-gnu-gcc make -j40 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
+CC=aarch64-linux-gnu-gcc make -j25 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
 if [ $? -ne 0 ]
 then
 	exit
 fi
-CC=aarch64-linux-gnu-gcc make -j40 ARCH=arm64 INSTALL_MOD_PATH=/home/boobee/VMmyinstall modules_install
+CC=aarch64-linux-gnu-gcc make -j25 ARCH=arm64 INSTALL_MOD_PATH=/home/boobee/VMmyinstall modules_install
 CC=aarch64-linux-gnu-gcc make ARCH=arm64 install
--- startVM.sh
+++ startVM.sh
@@ -2,8 +2,8 @@ qemu-system-aarch64 -machine virt -cpu cortex-a57 \
 	-machine type=virt -nographic -smp 2 \
 	-m 2048 \
 	-S \
-	-kernel /home/boobee/VMmyinstall/vmlinuz-5.14.0-00003-gdfdf4b60b9d8-dirty \
-	-initrd /home/boobee/VMmyinstall/myinitramfs-5.14.0.img.gz \
-	--append "mitigations=off nosmep nosmap console=ttyAMA0" \
+	-kernel /home/boobee/VMmyinstall/vmlinuz-5.14.0-00007-gaca3b16eae52-dirty \
+	-initrd /home/boobee/ARMport/myinitramfs-5.14.0.img.gz \
+	--append "mitigations=off console=ttyAMA0" \
 	-gdb tcp::5090
 	$1 $2
